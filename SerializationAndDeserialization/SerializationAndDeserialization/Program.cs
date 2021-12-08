66ca5ccb5b8380682b16239bf711f76ca4e85398
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-08 17:09:34 +0530

    [gunasekhar9] add : SerializationAndDeSerialization

---
+++ SerializationAndDeserialization/SerializationAndDeserialization/Program.cs
@@ -0,0 +1,18 @@
+﻿using SerializationAndDeserialization;
+using System;
+
+namespace BinarySerializationAndDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySerialization binary = new BinarySerialization();
+            //binary.Serialization();
+            binary.Deserialization();
+            JSONSerialization serialization = new JSONSerialization();
+            serialization.JsonSerialize();
+            serialization.JsonDeserialize();
+        }
+    }
+}
